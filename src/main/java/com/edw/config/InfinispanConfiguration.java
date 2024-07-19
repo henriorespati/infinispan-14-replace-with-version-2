bcd539f12d3033ddf4d028fef376bb7b190a6968
henriorespati <henrio.respati@gmail.com>
2024-07-19 14:53:58 +0700

    Update InfinispanConfiguration.java

--- src/main/java/com/edw/config/InfinispanConfiguration.java
+++ src/main/java/com/edw/config/InfinispanConfiguration.java
@@ -5,6 +5,7 @@ import org.infinispan.client.hotrod.configuration.ClientIntelligence;
 import org.infinispan.commons.marshall.ProtoStreamMarshaller;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Value;
 
 /**
  * <pre>
